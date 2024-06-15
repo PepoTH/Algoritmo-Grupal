2ef8a585406e2fddf1b3e3806b6c8a0bc26262f1
alejandrochmejia <alejandrochmejia@gmail.com>
2024-06-15 17:28:40 -0400

    Agregado de setters a la clase proyecto

--- sistema-gestion/proyectos.py
+++ sistema-gestion/proyectos.py
@@ -10,3 +10,14 @@ class proyecto:
         self.empresa = empresa
         self.gerente = gerente
         self.equipo = equipo
+
+    #Setters
+    def setId(self,id): self.id=id
+    def setNombre(self,nombre): self.nombre=nombre
+    def setDescripcion(self,descripcion): self.descripcion=descripcion
+    def setFechaInicio(self,fechaInicio): self.fechaInicio=fechaInicio
+    def setFechaVencimiento(self,fechaVencimiento): self.fechaVencimiento=fechaVencimiento
+    def setEstado(self,estado): self.estado=estado
+    def setEmpresa(self,empresa): self.empresa=empresa
+    def setGerente(self,gerente): self.gerente=gerente
+    def setEquipo(self,equipo): self.equipo=equipo
