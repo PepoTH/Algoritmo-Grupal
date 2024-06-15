7869f9fd547f85f3c0f50acd9dec7385406cdbfb
alejandrochmejia <alejandrochmejia@gmail.com>
2024-06-15 17:20:00 -0400

    Creacion de la inicializacion de proyecto

---
+++ sistema-gestion/proyectos.py
@@ -0,0 +1,12 @@
+#Clase para la unidad de proyecto
+class proyecto:
+    def __init__(self,id,nombre,descripcion,fechaInicio,fechaVencimiento,estado,empresa,gerente,equipo):
+        self.id = id
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.fechaInicio = fechaInicio
+        self.fechaVencimiento = fechaVencimiento
+        self.estado = estado
+        self.empresa = empresa
+        self.gerente = gerente
+        self.equipo = equipo
