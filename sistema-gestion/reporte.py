4fec7e590b821121c8ab6a9bb1668e910ec08753
PepoTH <cesardarizaleta@gmail.com>
2024-06-16 00:43:43 -0400

    Agregando funcion de busqueda de proyecto => Adaptar y Arreglar

--- sistema-gestion/reporte.py
+++ sistema-gestion/reporte.py
@@ -20,3 +20,25 @@ class reporte:
                         #Recorrido de Subtareas
                         print('\t'+ tarea.subtareas[k].nombre)
                 print('\n')
+
+    def proyectos(self,filtro):
+
+        #Arreglar los nombres de los atributos de ls proyectos
+        for i in range(0,len(self.proyectos)-1):
+            #Recorrido de Proyectos
+            if(self.proyectos[i].fechainit == filtro or self.proyectos[i].fechafin == filtro or self.proyectos[i].estado == filtro or self.proyectos[i].empresa == filtro):
+                #Verificar si el filtro concuerda con algun parametro
+                print('Nombre del proyecto: ',self.proyectos[i].nombre)
+                print('Descripcion del proyecto: ',self.proyectos[i].nombre)
+                print('Empresa del proyecto: ',self.proyectos[i].nombre)
+                print('Fecha de Inicio del proyecto: ',self.proyectos[i].nombre)
+                print('Fecha de Fin del proyecto: ',self.proyectos[i].nombre)
+                print('Porcentaje de Finalizacion: ',end='')
+                terminadas = 0
+                for j in range(0,len(self.proyectos[i].tareas) - 1):
+                    #Contador de Tareas completadas
+                    if(self.proyectos[i].tareas[j].estado == 'Completada'):
+                        terminadas += 1
+                #Calculo de Porcentaje respecto al total de tareas
+                porcentaje = terminadas / len(self.proyectos[i].tareas) * 100
+                print(porcentaje + '\n')
