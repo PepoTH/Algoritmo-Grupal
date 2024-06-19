ebd3b60b7271657c65716300fc163a7ded8a5c76
PepoTH <cesardarizaleta@gmail.com>
2024-06-18 21:09:17 -0400

    Cambios - Main

--- sistema-gestion/main.py
+++ sistema-gestion/main.py
@@ -1,2 +1,14 @@
 from proyectos import *
 
+def menu(opcion):
+
+    if(opcion == 1):
+        print('\tPROYECTOS\t')
+    elif(opcion == 2):
+        print('\tTAREAS\t')
+    elif(opcion == 3):
+        print('\tSUBTAREAS\t')
+    elif(opcion == 3):
+        print('\tCONSULTAS\t')
+
+
